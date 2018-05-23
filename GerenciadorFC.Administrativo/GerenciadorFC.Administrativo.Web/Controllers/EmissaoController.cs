b1599eb71aff8602f770ce4716302ab3a74a4948
Erivelto <fabioesimoes@gmail.com>
2018-05-22 21:33:54 -0300

    22/05/2018 00:00:00
    22/05/2018 00:00:00

---
+++ GerenciadorFC.Administrativo/GerenciadorFC.Administrativo.Web/Controllers/EmissaoController.cs
@@ -0,0 +1,40 @@
+﻿using GerenciadorFC.Administrativo.Web.Models.ContabilidadeViewModels.Faturamento;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GerenciadorFC.Administrativo.Web.Controllers
+{
+	public class EmissaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult DadosNotaFiscal()
+		{
+			var notaFiscalViewModels = new NotaFiscalViewModels();
+			return View(notaFiscalViewModels);
+		}
+		public async Task<IActionResult> IncluirDadosNfe(NotaFiscalViewModels notaFiscalViewModels)
+		{
+			using (var clientCont = new HttpClient())
+			{
+				clientCont.BaseAddress = new System.Uri("http://gerenciadorfccontabilidadeservico20180428013121.azurewebsites.net/api/NotaFiscal/" + notaFiscalViewModels.NumeroNFE.ToString());
+				var reposta = await clientCont.GetAsync("");
+				if (reposta.StatusCode.ToString() != "OK")
+				{
+					using (var clientContPost = new HttpClient())
+					{
+						clientContPost.BaseAddress = new System.Uri("https://gerenciadorfccontabilidadeservico20180428013121.azurewebsites.net/api/NotaFiscal");
+						var repostaPost = await clientContPost.PostAsJsonAsync("", notaFiscalViewModels);
+						var retornoPost = await repostaPost.Content.ReadAsStringAsync();
+					}
+				}
+			}
+
+			return View("DadosNotaFiscal");
+		}
+
+	}
+}
--- GerenciadorFC.Administrativo/GerenciadorFC.Administrativo.Web/Models/ContabilidadeViewModels/Faturamento/NotaFiscalViewModels.cs
+++ GerenciadorFC.Administrativo/GerenciadorFC.Administrativo.Web/Models/ContabilidadeViewModels/Faturamento/NotaFiscalViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,14 @@ namespace GerenciadorFC.Administrativo.Web.Models.ContabilidadeViewModels.Fatura
 		public int Codigo { get; set; }
 		public int CodigoPessoa { get; set; }
 		public DateTime DataEnvio { get; set; }
+		[Display(Name = "Data Emissão")]
+		[Required(ErrorMessage = "É obrigatorio data de emissão")]
 		public DateTime DataEmissao { get; set; }
+		[Display(Name = "Valor da nota fiscal")]
+		[Required(ErrorMessage = "É obrigatorio o valor total")]
 		public decimal ValorTotal { get; set; }
+		[Display(Name = "Número da nota fiscal")]
+		[Required(ErrorMessage = "É obrigatorio o numero da nota")]
 		public int NumeroNFE { get; set; }
 		public string CodigoVerificacao { get; set; }
 		public string UrlNfe { get; set; }
